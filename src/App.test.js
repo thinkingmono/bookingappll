2f16d744e8e475999afa0affa2080babc76c5661
Alejandro Garcia <thinkingmono92@gmail.com>
2023-07-27 23:13:50 -0500

    App.test.js | initializeTimes and updateTimes functions unit tests update to new conditions

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import { fireEvent, getAllByTestId, getByTestId, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import BookingForm from './components/booking/BookingForm';
 import BookingPage from './components/booking/BookingPage';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Book Form Test', () => {
   test('Renders the booking form heading', () => {
@@ -8,25 +9,17 @@ describe('Book Form Test', () => {
     const headingElement = screen.getByText("Make your reservation");
     expect(headingElement).toBeInTheDocument();
   });
-  test('Validate available times list change when date field change. First Option', () => {
-    const { getByTestId, getAllByTestId } = render(<BookingPage />);
-    const dateField = getByTestId('res-date');
-    expect(dateField).toBeInTheDocument();
-    fireEvent.change(dateField, {target: {value: '2023-07-29'}});
+  test('Check initializeTimes function to return an array with times', () => {
+    const { getAllByTestId } = render(<BrowserRouter><BookingPage /></BrowserRouter>);
     const availableTimes = getAllByTestId('available-time');
-    expect(availableTimes[0].value).toBe('17:00');
-    expect(availableTimes[1].value).toBe('18:00');
-    expect(availableTimes[2].value).toBe('19:00');
-    expect(availableTimes[3].value).toBe('20:00');
-    expect(availableTimes[4].value).toBe('21:00');
-    expect(availableTimes[5].value).toBe('22:00');
+    expect(availableTimes.length).toBeGreaterThan(0);
   });
-  test('Validate available times list change when date field change. Second Option', () => {
-    const { getByTestId, getAllByTestId } = render(<BookingPage />);
+  test('Check updateTimes function to return an array with times after date selection', () => {
+    const { getByTestId, getAllByTestId } = render(<BrowserRouter><BookingPage /></BrowserRouter>);
     const dateField = getByTestId('res-date');
     expect(dateField).toBeInTheDocument();
-    fireEvent.change(dateField, {target: {value: '2023-07-19'}});
+    fireEvent.change(dateField, { target: { value: '2023-07-19' } });
     const availableTimes = getAllByTestId('available-time');
-    expect(availableTimes[0].value).toBe('08:00');
+    expect(availableTimes.length).toBeGreaterThan(0);
   });
 });
