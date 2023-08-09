64aa731f4bc4205bd43442f699d999f3c197a6c2
Alejandro Garcia <thinkingmono92@gmail.com>
2023-08-08 22:22:57 -0500

    Header.js | Toggle side mobile menu addition. Show menu controlled by useState hook and onClick Event on hamburguer icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import Nav from './Nav';
 import logo from '../../assets/img/Logo.svg';
 import menu from '../../assets/img/icon _hamburger menu.svg';
 import basket from '../../assets/img/basket .svg';
 
 function Header() {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <>
             <header>
@@ -12,9 +15,17 @@ function Header() {
                     <Nav />
                 </div>
                 <div className="app-header-mobile">
-                    <img src={menu} alt="Hamburguer Mobile Menu" className="" />
-                    <img src={logo} alt="Little Lemon's Logo" className="" />
-                    <img src={basket} alt="Basket Icon" className="" />
+                    <div className='menu-mobile'>
+                        <img src={menu} alt="Hamburguer Mobile Menu" className="hamburguer-menu-icon" onClick={() => setShowMenu(!showMenu)}/>
+                        {showMenu ? (
+                            <div className='side-menu'>
+                                <span className='close-btn' onClick={() => setShowMenu(false)}>X</span>
+                                <Nav />
+                            </div>
+                        ) : null}
+                    </div>
+                    <img src={logo} alt="Little Lemon's Logo" className="logo-mobile" />
+                    <img src={basket} alt="Basket Icon" className="basket-mobile" />
                 </div>
             </header>
         </>
